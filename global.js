f3cd46911066634bffc8853919a0de4a9a4e8a9a
Hallo89 <karl.leiser@yahoo.com>
2018-02-12 17:30:07 +0100

    R E S P O N S I V E  B A C K G R O U N D  B L U R

--- global.js
+++ global.js
@@ -5,19 +5,18 @@ function toggleMode() {
 }
 //theme buttons dropdown
 function toggleDropdown() {
-    document.getElementById('mode_dropdown').classList.toggle('enabled');
-}
-window.onclick = function(event) {
-  if (!event.target.matches (
-    [
-      '.fa-cog',
-      '.mode_switch',
-      '.mode_dropdown_box'
-    ]
-  ))
-  {
-    if (document.getElementById('mode_dropdown').classList.contains('enabled')) {
-      document.getElementById('mode_dropdown').classList.remove('enabled');
+  document.getElementById('mode_dropdown').classList.toggle('enabled');
+  window.onclick = function(event) {
+    if (!event.target.matches (
+      [
+        '.fa-cog',
+        '.mode_switch'
+      ]
+    ))
+    {
+      if (document.getElementById('mode_dropdown').classList.contains('enabled')) {
+        document.getElementById('mode_dropdown').classList.remove('enabled');
+      }
     }
   }
 }
