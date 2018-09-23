0048135f7081cf25b1047db84e534d4d1051b37f
Hallo89 <karl.leiser@yahoo.com>
2018-09-23 22:05:31 +0200

    Homepage major rework: boxes, headline, light mode, responsive header, overall improvements

--- global.js
+++ global.js
@@ -1,5 +1,8 @@
 const headline = document.getElementById('headline');
-const nav = document.getElementById('head_nav');
+const nav = document.querySelector('.head_nav');
+const modes = document.querySelector('.modes');
+
+const headlineRect = headline.getBoundingClientRect();
 //but why
 //The solution for the hack to position a relative element vertically centered, which is top: 50%; transform: translateY(-50%); -> checks whether css grid is supported, if it's not (which is when the hack should be active), replace the modular translateY with the static one for the hack
 //Tho I am experimentally using a static top value
@@ -7,11 +10,12 @@ const nav = document.getElementById('head_nav');
 //checkCSSGrid.style.display = 'grid';
 const shiftCapX = 6;
 const shiftCapY = 2;
+
 headline.addEventListener('mousemove', navWiggle);
+
 function navWiggle(e) {
-  const rect = this.getBoundingClientRect();
-  let left = e.x - rect.left;
-  let top = e.y - rect.top;
+  let left = e.x - headlineRect.left;
+  let top = e.y - headlineRect.top;
   let shiftLeft = Math.round((left / headline.offsetWidth * shiftCapX) - (shiftCapX / 2));
   let shiftTop = Math.round((top / headline.offsetHeight * shiftCapY) - (shiftCapY / 2));
   /*if (checkCSSGrid.style.display == 'grid') {*/
@@ -33,21 +37,17 @@ function toggleHeadline() {
 }
 //theme buttons dropdown
 function toggleDropdown() {
-  document.getElementById('modes').classList.toggle('enabled');
-  window.onclick = function(event) {
-    if (!event.target.matches (
-      [
-        '.mode_gear',
-        '.dropdown_box',
-        '.dropdown_header',
-        '.dropdown_header_text',
-        '.mode_switch'
-      ]
-    ))
-    {
-      if (document.getElementById('modes').classList.contains('enabled')) {
-        document.getElementById('modes').classList.remove('enabled');
-      }
-    }
+  modes.classList.toggle('enabled');
+}
+
+window.onclick = function(e) {
+  if (modes.classList.contains('enabled') && !e.target.matches ([
+    '.mode_gear',
+    '.dropdown_box',
+    '.dropdown_header',
+    '.dropdown_header_text',
+    '.mode_switch'
+  ])) {
+    modes.classList.remove('enabled');
   }
 }
