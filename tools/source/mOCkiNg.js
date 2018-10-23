fce1ff0b329671db211c1c8b5ed66d63e20a526a
Hallo89 <karl.leiser@yahoo.com>
2018-10-23 19:06:44 +0200

    Remove ancient stuff and ignore line breaks for maximum awesomeness

--- tools/source/mOCkiNg.js
+++ tools/source/mOCkiNg.js
@@ -1,12 +1,6 @@
-//predefined file-global variables
-var request = new XMLHttpRequest(); //The core of the program generating the structure to send requests to the API
-
 //consts filled with elements which are used in this file
 const input = document.querySelector('.input_box.text_input .input.text'); //The input textarea everything begins with
 const output = document.getElementById('output');
-const box = document.querySelector('.box');
-const header = document.querySelector('.box .box_header');
-const input2 = document.querySelector('.box .input_container:nth-child(3)');
 
 //Event listeners
 input.addEventListener('input', resizeInput);
@@ -17,23 +11,24 @@ resizeInput();
 
 //functions
 function mockingText() {
-  let value = input.value;
-  if (value == '') {
+  let inputValue = input.value;
+  if (inputValue == '') {
     output.innerHTML = '';
     return;
   }
+  let processValue = inputValue.replace(/\n/g, '');
   let outputValue = '';
-  for (i = 0; i < value.length; i++) {
+  for (let i = 0; i < processValue.length; i++) {
     if (i > 1 && outputValue[i-1] == outputValue[i-1].toUpperCase() && outputValue[i-2] == outputValue[i-2].toUpperCase()) {
-      outputValue += value[i].toLowerCase();
+      outputValue += processValue[i].toLowerCase();
     } else if (i > 1 && outputValue[i-1] == outputValue[i-1].toLowerCase() && outputValue[i-2] == outputValue[i-2].toLowerCase()) {
-      outputValue += value[i].toUpperCase();
+      outputValue += processValue[i].toUpperCase();
     } else {
-      outputValue += Math.round(Math.random()) == 1 ? value[i].toUpperCase() : value[i].toLowerCase();
+      outputValue += Math.round(Math.random()) == 1 ? processValue[i].toUpperCase() : processValue[i].toLowerCase();
     }
   }
-  while (outputValue.indexOf('\n') >= 0) {
-    outputValue = outputValue.replace('\n', '<br>');
+  for (let i = 0; i < inputValue.match(/\n/g).length; i++) {
+    outputValue = outputValue.slice(0, inputValue.indexOf('\n') + 5*i) + '<br>' + outputValue.slice(inputValue.indexOf('\n') + 5*i);
   }
   output.innerHTML = outputValue;
 }
