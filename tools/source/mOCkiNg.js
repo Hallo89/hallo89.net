1599dc728c29f5d4d7c4952f5473b81945b435fe
Hallo89 <hallo89@protonmail.com>
2018-11-28 19:28:22 +0100

    Improve mocking to ignore all sorts of special characters

--- tools/source/mOCkiNg.js
+++ tools/source/mOCkiNg.js
@@ -20,19 +20,19 @@ function mockingText() {
   for (let i = 0; i < inputValue.length; i++) {
     let ln1 = i-1;
     let ln2 = i-2;
-    while (true) {
-      if (inputValue[ln1] == '\n') {
+    while (i > 1) {
+      if (inputValue[ln1] && !inputValue[ln1].hasCasing()) {
         ln1--;
         ln2--;
       }
-      if (inputValue[ln2] == '\n') {
+      if (inputValue[ln2] && !inputValue[ln2].hasCasing()) {
         ln2--;
       }
-      if (inputValue[ln1] != '\n' && inputValue[ln2] != '\n') {
+      if (!inputValue[ln2] || !inputValue[ln1] || inputValue[ln1].hasCasing() && inputValue[ln2].hasCasing()) {
         break;
       }
     }
-    if (inputValue[i] != '\n') {
+    if (inputValue[i].hasCasing()) {
       outputValue += mockify(inputValue, outputValue, i, ln1, ln2);
     } else {
       outputValue += inputValue[i];
@@ -43,9 +43,9 @@ function mockingText() {
 }
 
 function mockify(inputText, outputText, index1, index2, index3) {
-  if (outputText[index2] && outputText[index3] && outputText[index2] == outputText[index2].toUpperCase() && outputText[index3] == outputText[index3].toUpperCase()) {
+  if (outputText[index2] && outputText[index3] && outputText[index2].isUpperCase() && outputText[index3].isUpperCase()) {
     return inputText[index1].toLowerCase();
-  } else if (outputText[index2] && outputText[index3] && outputText[index2] == outputText[index2].toLowerCase() && outputText[index3] == outputText[index3].toLowerCase()) {
+  } else if (outputText[index2] && outputText[index3] && outputText[index2].isLowerCase() && outputText[index3].isLowerCase()) {
     return inputText[index1].toUpperCase();
   } else {
     return Math.round(Math.random()) == 1 ? inputText[index1].toUpperCase() : inputText[index1].toLowerCase();
@@ -62,3 +62,15 @@ function recordEnter(e) {
     mockingText();
   }
 }
+
+String.prototype.hasCasing = function() {
+  return this.toUpperCase() != this.toLowerCase();
+};
+
+String.prototype.isLowerCase = function() {
+  return this == this.toLowerCase();
+};
+
+String.prototype.isUpperCase = function() {
+  return this == this.toUpperCase();
+};
