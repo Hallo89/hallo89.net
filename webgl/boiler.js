8b36f49b8ba6b64a0314599bba6bebc4e3e392a8
Hallo89 <hallo89@protonmail.com>
2018-12-08 00:11:00 +0100

    Improve the webgl boilerplate for better structure & functionality

--- webgl/boiler.js
+++ webgl/boiler.js
@@ -7,46 +7,42 @@
 const canvas = document.querySelector('canvas'); //The body of this, being the thing I work with
 
 //The absolute legend being the heart of everything (literally everything)
-var gl;
-if (preserveBuffer) {
-  gl = canvas.getContext('webgl2', {preserveDrawingBuffer: true});
-} else {
-  gl = canvas.getContext('webgl2');
-}
+const gl = canvas.getContext('webgl2', {preserveDrawingBuffer: preserveBuffer});
 if (!gl) {
   console.error('It seems like WebGL2 is not supported');
-  document.body.innerHTML = 'It seems like WebGL2 is not supported';
+  document.body.innerHTML = '<span style="color: hsl(0, 0%, 80%); font-size: 50px;">It seems like WebGL2 is not supported</span>';
 }
 
+const webgl = {
+  //The function compiling either one of the shaders, where 'shaderSource' is obvious and shaderType is either FRAGMENT_SHADER or VERTEX_SHADER
+  compileShader: function(source, type) {
+   //creating a new empty 'shader hull'
+    let shader = gl.createShader(type);
+   //linking the shader code to the just created empty shader and compiling it
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+   //get the status. If not correct, throw an error
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.error('Error: could not compile shader: ' + gl.getShaderInfoLog(shader));
+    }
+   //return the now compiled shader, which is only either one
+    return shader;
+  },
 
-//The function compiling either one of the shaders, where 'shaderSource' is obvious and shaderType is either FRAGMENT_SHADER or VERTEX_SHADER
-function compileShader(source, type) {
- //creating a new empty 'shader hull'
-  let shader = gl.createShader(type);
- //linking the shader code to the just created empty shader and compiling it
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
- //get the status. If not correct, throw an error
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error('Error: could not compile shader: ' + gl.getShaderInfoLog(shader));
+  //The function creating a 'program' from both needed shaders
+  constructProgram: function(vertex, fragment) {
+   //creating an empty program to be written
+    let program = gl.createProgram();
+   //attaching both shaders to the empty program
+    gl.attachShader(program, this.compileShader(vertex, gl.VERTEX_SHADER));
+    gl.attachShader(program, this.compileShader(fragment, gl.FRAGMENT_SHADER));
+   //linking the program. I don't know what this does. I think it links, as in merges, both just-attached shaders together into the final thing
+    gl.linkProgram(program);
+   //get the LINK_STATUS which is the status (mind = blown). If not correct, throw an error
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.error('Error: program failed to link: ' + gl.getProgramInfoLog(program));
+    }
+   //return the now finished (as in linked) program
+    return program;
   }
- //return the now compiled shader, which is only either one
-  return shader;
 }
-
-//The function creating a 'program' from both needed shaders
-function createProgram() {
- //creating an empty program to be written
-  let program = gl.createProgram();
- //attaching both shaders to the empty program
-  gl.attachShader(program, compileShader(vertexSource, gl.VERTEX_SHADER));
-  gl.attachShader(program, compileShader(fragmentSource, gl.FRAGMENT_SHADER));
- //linking the program. I don't know what this does. I think it links, as in merges, both just-attached shaders together into the final thing
-  gl.linkProgram(program);
- //get the LINK_STATUS which is the status (mind = blown). If not correct, throw an error
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error('Error: program failed to link: ' + gl.getProgramInfoLog(program));
-  }
- //return the now finished (as in linked) program
-  return program;
-};
