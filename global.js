427ab9f6613e4db4300b069fdbea1c5f3ee5d005
Hallo89 <hallo89@protonmail.com>
2018-12-07 21:16:12 +0100

    Firefox 64 styled scrollbars; modes now on html, clean up global.js

--- global.js
+++ global.js
@@ -1,13 +1,10 @@
+const html = document.querySelector('html');
 const headline = document.getElementById('headline');
 const nav = document.querySelector('.head_nav');
 const modes = document.querySelector('.modes');
 
 const headlineRect = headline.getBoundingClientRect();
-//but why
-//The solution for the hack to position a relative element vertically centered, which is top: 50%; transform: translateY(-50%); -> checks whether css grid is supported, if it's not (which is when the hack should be active), replace the modular translateY with the static one for the hack
-//Tho I am experimentally using a static top value
-//const checkCSSGrid = document.createElement('div');
-//checkCSSGrid.style.display = 'grid';
+
 const shiftCapX = 6;
 const shiftCapY = 2;
 
@@ -18,18 +15,12 @@ function navWiggle(e) {
   let top = e.y - headlineRect.top;
   let shiftLeft = Math.round((left / headline.offsetWidth * shiftCapX) - (shiftCapX / 2));
   let shiftTop = Math.round((top / headline.offsetHeight * shiftCapY) - (shiftCapY / 2));
-  /*if (checkCSSGrid.style.display == 'grid') {*/
-    nav.style.transform = 'translateX(' + -shiftLeft + 'px) translateY(' + -shiftTop + 'px)';
-/*  }
-  else {
-    nav.style.transform = 'translateX(' + -shiftLeft + 'px) translateY(-50%)';
-    console.log("ok2");
-  }*/
+  nav.style.transform = 'translateX(' + -shiftLeft + 'px) translateY(' + -shiftTop + 'px)';
 }
 //theme changer
 function toggleMode() {
-  document.body.classList.toggle('dark-mode');
-  document.body.classList.toggle('light-mode');
+  html.classList.toggle('dark-mode');
+  html.classList.toggle('light-mode');
 }
 //expand/hide the headline
 function toggleHeadline() {
