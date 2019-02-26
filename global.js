686d0619fcc4f7dd3ca79b9b2eed10d8ce934cb8
Hallo89 <hallo89@protonmail.com>
2019-02-26 21:28:25 +0100

    Major headline revamp; removal on main page, integration on tutorials
    + little less style changes

--- global.js
+++ global.js
@@ -1,23 +1,7 @@
 const html = document.documentElement;
-const headline = document.getElementById('headline');
-const nav = document.querySelector('.head_nav');
-const modes = document.querySelector('.modes');
+const headline = document.getElementById('navbar');
+const modes = document.getElementById('modes');
 
-const headlineRect = headline.getBoundingClientRect();
-
-const shiftCapX = 6;
-const shiftCapY = 2;
-
-headline.addEventListener('mousemove', navWiggle);
-
-function navWiggle(e) {
-  let left = e.x - headlineRect.left;
-  let top = e.y - headlineRect.top;
-  let shiftLeft = Math.round((left / headline.offsetWidth * shiftCapX) - (shiftCapX / 2));
-  let shiftTop = Math.round((top / headline.offsetHeight * shiftCapY) - (shiftCapY / 2));
-  nav.style.transform = 'translateX(' + -shiftLeft + 'px) translateY(' + -shiftTop + 'px)';
-}
-//theme changer
 function toggleMode() {
   html.classList.toggle('dark-mode');
   html.classList.toggle('light-mode');
