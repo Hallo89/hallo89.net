abb9860a03c220449f5405ee6d3e6b85d583e125
Hallo89 <hallo89@protonmail.com>
2019-08-03 22:04:39 +0200

    Correctly fetch github data

--- index.js
+++ index.js
@@ -113,47 +113,21 @@ get('webgl/matrices3d');
 
 const jsonData = require('./source/resources/slider89/docs.json');
 let gitData = [];
-// fetch('https://api.github.com/repos/Hallo89/Slider89/releases')
-//   .then(res => res.json())
-//   .then(data => {
-//     gitData = data;
-//     for (version of gitData) {
-//       version.body = (function() {
-//         return markdown.render(version.body);
-//       })();
-//       version.date = (function() {
-//         const match = version.created_at.match(/(\d{4})-(\d{2})-(\d{2})/);
-//         return (match != null ? match[3] + '/' + match[2] + '/' + match[1] : val);
-//       })();
-//     }
-//   });
-gitData = [
-  {
-    tag_name: 'v0.2.0',
-    body: "- New property: `structure`. It takes a string similar to an XML structure to assemble a customized HTML structure (-> [docs](https://hallo89.net/slider89#property-structure))\r\n- `element` has been changed in favor of the new structure system: It is now a JavaScript object containing every part of the slider as opposed to only the root element (-> [docs](https://hallo89.net/slider89#property-element))\r\n- `TipDuration` can now be set to `false` to prevent it from disappearing altogether\r\n- The HTML classing has been changed to improve stylability: non crucial CSS suited for altering, has been moved into a new, independent class to be spoken to without nesting in the selector, `slider89_{slider-part}`; crucial CSS is still selected with a (more difficult to override) `.slider89 .slider_{slider-part}`",
-    created_at: "2018-12-31T04:13:19Z"
-  },
-  {
-    tag_name: 'v0.1.1',
-    body: "- New property: `TaskMouseUp`, taking a function to be executed every time the mouse is released after engaging with a slider possessing this property\r\n- `value` is now trimmed to the value of `comma`, when specified, within the same initialization\r\n- If `value` is changed to more than `max` or less than `min` with `newValues()`, they are now set to min and max accordingly\r\n- Fixed the width not computing anew when changing values with `newValues()`\r\n- Fixed new `min`, `max` and `comma`  values being ignored when they are changed to 0 in `newValues()`",
-    created_at: "2018-12-07T23:01:53Z"
-  },
-  {
-    tag_name: 'v0.1.0',
-    body: "The Slider89's initial release. More updates will come in the future.",
-    created_at: "2018-11-11T15:21:23Z"
-  }
-];
+fetch('https://api.github.com/repos/Hallo89/Slider89/releases')
+  .then(res => res.json())
+  .then(data => {
+    gitData = data;
+    for (version of gitData) {
+      version.body = (function() {
+        return markdown.render(version.body);
+      })();
+      version.date = (function() {
+        const match = version.created_at.match(/(\d{4})-(\d{2})-(\d{2})/);
+        return (match != null ? match[3] + '/' + match[2] + '/' + match[1] : val);
+      })();
+    }
+  });
 
-for (version of gitData) {
-  version.body = (function() {
-    return markdown.render(version.body);
-  })();
-  version.date = (function() {
-    const match = version.created_at.match(/(\d{4})-(\d{2})-(\d{2})/);
-    return (match != null ? match[3] + '/' + match[2] + '/' + match[1] : val);
-  })();
-}
 app.get('/slider89', function(req, res) {
   res.render('slider89', {page: 'slider89', data: jsonData, gitData: gitData});
 });
