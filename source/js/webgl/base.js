53e7613a55ae754dfa43498849f483279e827008
Hallo89 <hallo89@protonmail.com>
2019-08-14 22:20:27 +0200

    Improve base.js for webGL experiments

--- source/js/webgl/base.js
+++ source/js/webgl/base.js
@@ -1,24 +1,16 @@
 const toolbar = document.querySelector('.toolbar');
 
-const crementButtonsUp = document.querySelectorAll('.crement.plus');
-const crementButtonsDown = document.querySelectorAll('.crement.minus');
-const crementContainer = document.querySelectorAll('.crement_buttons');
+const crementBtnsUp = document.querySelectorAll('.crement.plus');
+const crementBtnsDown = document.querySelectorAll('.crement.minus');
+const crementContainer = document.getElementsByClassName('crement_buttons');
 
 const inputsChecker = document.querySelectorAll('.input_box.checker .input.checker');
 
 
-for (var i = 0; i < crementContainer.length; i++) {
-  crementContainer[i].addEventListener('wheel', crementScroll);
-}
-for (var i = 0; i < crementButtonsUp.length; i++) {
-  crementButtonsUp[i].addEventListener('click', increment);
-}
-for (var i = 0; i < crementButtonsDown.length; i++) {
-  crementButtonsDown[i].addEventListener('click', decrement);
-}
-for (var i = 0; i < inputsChecker.length; i++) {
-  inputsChecker[i].addEventListener('click', handleChecker);
-}
+for (l of crementContainer) l.addEventListener('wheel', crementScroll);
+for (l of crementBtnsUp) l.addEventListener('click', increment);
+for (l of crementBtnsDown) l.addEventListener('click', decrement);
+for (l of inputsChecker) l.addEventListener('click', handleChecker);
 window.addEventListener('mousemove', handleMouseMove);
 
 
@@ -36,48 +28,26 @@ function handleChecker() {
 
 function increment() {
   const input = this.parentNode.previousElementSibling;
-  if (input.max != '' && parseInt(input.value) < parseInt(input.max)) {
-    input.value++;
-  } else if (input.max == '') {
-    input.value++;
-  }
+  if (input.max != '' && parseInt(input.value) < parseInt(input.max) || input.max == '') input.value++;
   input.focus();
-  if (input.classList.contains('rgb')) {
-    rgbChange();
-  }
+  if (input.classList.contains('rgb')) rgbChange();
 }
 function decrement() {
   const input = this.parentNode.previousElementSibling;
-  if (input.min != '' && parseInt(input.value) > parseInt(input.min)) {
-    input.value--;
-  } else if (input.min == '') {
-    input.value--;
-  }
+  if (input.min != '' && parseInt(input.value) > parseInt(input.min) || input.min == '') input.value--;
   input.focus();
-  if (input.classList.contains('rgb')) {
-    rgbChange();
-  }
+  if (input.classList.contains('rgb')) rgbChange();
 }
 function crementScroll(e) {
-  let scrollY = e.deltaY;
+  const scrollY = e.deltaY;
   if (scrollY != 0) {
     const input = this.parentNode.children[0];
     if (scrollY > 0) {
-      if (input.min != '' && parseInt(input.value) > parseInt(input.min)) {
-        input.value--;
-      } else if (input.min == '') {
-        input.value--;
-      }
+      if (input.min != '' && parseInt(input.value) > parseInt(input.min) || input.min == '') input.value--;
     } else if (scrollY < 0) {
-      if (input.max != '' && parseInt(input.value) < parseInt(input.max)) {
-        input.value++;
-      } else if (input.max == '') {
-        input.value++;
-      }
+      if (input.max != '' && parseInt(input.value) < parseInt(input.max) || input.max == '') input.value++;
     }
     input.focus();
-    if (input.classList.contains('rgb')) {
-      rgbChange();
-    }
+    if (input.classList.contains('rgb')) rgbChange();
   }
 }
