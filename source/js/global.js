5f3dd0d43b404a0d616408df8ed6a824cdcd2635
Hallo89 <hallo89@protonmail.com>
2021-02-06 23:09:36 +0100

    Implement custom routing of component box quick links
    + global.js code rearrangements

--- source/js/global.js
+++ source/js/global.js
@@ -1,25 +1,38 @@
-function getCookie(name) {
-  const list = document.cookie.match('(?:;\\s*|^)' + name + '=(.+?)(?:;|$)');
-  return list != null ? list[1] : null;
-}
-
 const hasAccepted = getCookie('acceptedCookies');
-
-//Create the banner element
 const banner = (function() {
   const node = document.createElement('div');
   node.innerHTML = '<aside class="glb-box" id="cookiebanner"><b class="background"></b><div class="description box-text">This site uses cookies by Cloudflare, this banner and to save your preferenced theme color. By continuing to use it, you agree to them!</div><button type="button" onclick="hideBanner()">Alrighty!</button></aside>';
   return node.firstChild;
 })();
 
-if (hasAccepted != 'true') {
-  //If no cookie saying that cookies have been accepted is present, append the banner to the body
-  window.addEventListener('load', function() {
+window.addEventListener('load', function() {
+  //add a custom href to the buttons in component boxes (since anchors can't be nested)
+  const componentBoxes = document.querySelectorAll('.e-glb-box.glb-components');
+  for (var i = 0; i < componentBoxes.length; i++) {
+    const box = componentBoxes[i];
+    const quickLinkBtns = box.querySelectorAll('.quick-links button');
+
+    for (var n = 0; n < quickLinkBtns.length; n++) {
+      const button = quickLinkBtns[n];
+      button.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.href = this.dataset.href;
+      });
+    }
+  }
+
+  //add the cookie banner if cookies haven't been accepted
+  if (hasAccepted != 'true') {
     document.body.insertBefore(banner, document.body.children[0]);
     setTimeout(function() {
       banner.classList.remove('invisible');
     }, 10);
-  });
+  }
+});
+
+function getCookie(name) {
+  const list = document.cookie.match('(?:;\\s*|^)' + name + '=(.+?)(?:;|$)');
+  return list != null ? list[1] : null;
 }
 
 function hideBanner() {
