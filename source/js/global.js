Hallo89/hallo89.net

commit 03bca69a7fd3618b0331ce8179d7f985d127c5a7

Make cookies SameSite=strict + secure (HTTPS), simplify js banner HTML

source/js/global.js
@@ -7,24 +7,9 @@ const hasAccepted = getCookie('acceptedCookies');
 
 //Create the banner element
 const banner = (function() {
-  const aside = document.createElement('aside');
-  const bg = document.createElement('b');
-  const text = document.createElement('div');
-  const btn = document.createElement('button');
-  aside.classList.add('glb-box');
-  aside.classList.add('invisible');
-  bg.classList.add('background');
-  text.classList.add('description');
-  text.classList.add('box-text');
-  aside.id = 'cookiebanner';
-  btn.type = 'button';
-  text.textContent = 'This site uses cookies by Cloudflare and this very banner. By continuing to use it, you agree to them!';
-  btn.textContent = 'Alrighty!';
-  btn.addEventListener('click', hideBanner);
-  aside.appendChild(bg);
-  aside.appendChild(text);
-  aside.appendChild(btn);
-  return aside;
+  const node = document.createElement('div');
+  node.innerHTML = '<aside class="glb-box" id="cookiebanner"><b class="background"></b><div class="description box-text">This site uses cookies by Cloudflare, this banner and to save your preferenced theme color. By continuing to use it, you agree to them!</div><button type="button" onclick="hideBanner()">Alrighty!</button></aside>';
+  return node.firstChild;
 })();
 
 if (hasAccepted != 'true') {
@@ -48,6 +33,6 @@ function hideBanner() {
       }, 200);
     }, 80);
   }, 450);
-  document.cookie = 'acceptedCookies=true; path=/';
+  document.cookie = 'acceptedCookies=true; path=/; secure; SameSite=strict';
   this.removeEventListener('click', hideBanner);
 }

source/js/global-themes.js
@@ -35,5 +35,5 @@ function toggleMode(mode) {
 }
 
 function setThemeCookie() {
-  document.cookie = 'theme=' + (html.classList.contains('light-mode') ? 'light' : 'dark') + '; path=/';
+  document.cookie = 'theme=' + (html.classList.contains('light-mode') ? 'light' : 'dark') + '; path=/; secure; SameSite=strict';
 }

commit 3a3586a9611831c574625b5805a00c5f1275727f

Fix cookie banner containing the wrong content, better declaration style
This bug was introduced in 03bca69 when I converted the creation from 
createElement to innerHTML

source/js/global.js
@@ -1,7 +1,14 @@
 const hasAccepted = getCookie('acceptedCookies');
 const banner = (function() {
   const node = document.createElement('div');
-  node.innerHTML = '<aside class="glb-box" id="cookiebanner"><b class="background"></b><div class="description box-text">This site uses cookies by Cloudflare, this banner and to save your preferenced theme color. By continuing to use it, you agree to them!</div><button type="button" onclick="hideBanner()">Alrighty!</button></aside>';
+  node.innerHTML =
+    '<aside id="cookiebanner">' +
+      '<b class="background"></b>' +
+      '<div class="description box-text">' +
+        'This site uses cookies by Cloudflare and this very banner. By continuing to use it, you agree to them!' +
+      '</div>' +
+      '<button type="button" onclick="hideBanner()">Alrighty!</button>' +
+    '</aside>';
   return node.firstChild;
 })();
 