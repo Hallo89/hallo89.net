c7e848899cd7cf3d361a32139d0b963ea6c6b77e
Hallo89 <hallo89@protonmail.com>
2022-08-05 17:22:55 +0200

    Convert Controls3D into an ES6 class

--- source/js/graphics/Controls3D.js
+++ source/js/graphics/Controls3D.js
@@ -1,232 +1,215 @@
 'use strict';
-var Controls3D = (function() {
-  let hasGamepad = false;
-  const gamepads = {};
+class Controls3D {
+  _hasGamepad = false;
+  _gamepads = {};
 
-  function Controls3D(canvas, drawFunction, skipEvents) {
-    const that = this;
+  // Persistant transform properties, here initial
+  _clickState = {};
+  _clickedBtn;
 
-    // persistant transform properties, here initial
-    const clickState = {};
-    let clickedBtn;
+  // Animation helper properties
+  animationID = {
+    scale: -1,
+    tran: -1,
+    rot: -1
+  };
+  animationInitial = {
+    scale: null,
+    tran: null,
+    rot: null
+  };
 
-    that.drawFunction = drawFunction;
-    that.animationID = {
-      scale: -1,
-      tran: -1,
-      rot: -1
-    };
-    that.animationInitial = {
-      scale: null,
-      tran: null,
-      rot: null
-    };
+  // Configurable properties
+  drawFunction;
 
-    that.joystickThreshold = .14;
+  joystickThreshold = .14;
 
-    that.mod = {
-      scale: .224,
-      tran: .025,
-      rot: .44
-    };
-    that.gamepadMod = {
-      scale: .015,
-      tran: .25,
-      rot: .75
-    };
+  mod = {
+    scale: .224,
+    tran: .025,
+    rot: .44
+  };
+  gamepadMod = {
+    scale: .015,
+    tran: .25,
+    rot: .75
+  };
 
-    that.state = {
-      scale: {
-        x: 1,
-        y: 1,
-        z: 1
-      },
-      tran: {
-        x: 0,
-        y: 0,
-        z: 0
-      },
-      rot: {
-        x: 0,
-        y: 0,
-        z: 0 //not implemented yet
-      }
-    };
+  // State
+  state = {
+    scale: {
+      x: 1,
+      y: 1,
+      z: 1
+    },
+    tran: {
+      x: 0,
+      y: 0,
+      z: 0
+    },
+    rot: {
+      x: 0,
+      y: 0,
+      z: 0 //not implemented yet
+    }
+  };
+
+  constructor(canvas, drawFunction, skipEvents) {
+    this.drawFunction = drawFunction;
 
     canvas.addEventListener('contextmenu', e => {
       e.preventDefault();
     });
     if (!skipEvents) {
-      canvas.addEventListener('pointerdown', mouseDown);
-      window.addEventListener('pointerup', removeMouseMove);
-      canvas.addEventListener('wheel', wheel); //TODO: support for mousewheel
+      // This permanently binds the methods to `this` (e.g. to be able to remove the event)
+      this.mouseMove = this.mouseMove.bind(this);
+      this.gamepadLoop = this.gamepadLoop.bind(this);
 
-      window.addEventListener('gamepadconnected', gamepadConnected.bind(that));
-      window.addEventListener('gamepaddisconnected', gamepadDisconnected.bind(that));
-    }
+      canvas.addEventListener('pointerdown', this.mouseDown.bind(this));
+      window.addEventListener('pointerup', this.removeMouseMove.bind(this));
+      canvas.addEventListener('wheel', this.wheel.bind(this)); //TODO: support for mousewheel
 
-
-    // ---- GamepadEvent functions ----
-    function gamepadConnected(e) {
-      gamepads[e.gamepad.index] = e.gamepad;
-      hasGamepad = true;
-      gamepadLoop();
-    }
-    function gamepadDisconnected(e) {
-      delete gamepads[e.gamepad.index];
-      if (Object.keys(gamepads).length == 0) {
-        hasGamepad = false;
-      }
-    }
-
-    function gamepadLoop() {
-      for (const gamepadIndex in gamepads) {
-        const gamepad = gamepads[gamepadIndex];
-
-        // Scale, LB/RB
-        if (gamepad.buttons[4].pressed || gamepad.buttons[5].pressed) {
-          const direction = gamepad.buttons[5].value || -gamepad.buttons[4].value;
-          if (direction) {
-            that.animateStates(35, {
-              scale: {
-                x: direction * that.gamepadMod.scale,
-                y: direction * that.gamepadMod.scale,
-                z: direction * that.gamepadMod.scale
-              }
-            }, undefined, undefined, true);
-          }
-        }
-
-        // Translate, left joystick
-        handleJoystick([gamepad.axes[0], -gamepad.axes[1]], gamepad.buttons[10], 'tran');
-
-        // Rotate, right joystick
-        handleJoystick([gamepad.axes[3], gamepad.axes[2]], gamepad.buttons[11], 'rot');
-      }
-
-      if (hasGamepad) {
-        requestAnimationFrame(gamepadLoop);
-      }
-    }
-
-    function handleJoystick(axes, resetButton, action) {
-      let hasNewState = false;
-      const newState = {};
-      newState[action] = {};
-
-      if (axes[0] > that.joystickThreshold || axes[0] < -that.joystickThreshold) {
-        hasNewState = true;
-        newState[action].x = (axes[0] * that.gamepadMod[action]) + that.state[action].x;
-      }
-      if (axes[1] > that.joystickThreshold || axes[1] < -that.joystickThreshold) {
-        hasNewState = true;
-        newState[action].y = (axes[1] * that.gamepadMod[action]) + that.state[action].y;
-      }
-
-      if (hasNewState) {
-        that.assignNewStateAndDraw(newState);
-      }
-
-      // Reset distance when right joystick is pressed
-      if (resetButton.pressed) {
-        newState[action].x = 0;
-        newState[action].y = 0;
-        that.assignNewStateAndDraw(newState);
-      }
-    }
-
-    // ---- MouseEvent functions ----
-    async function wheel(e) {
-      if (e.ctrlKey) e.preventDefault();
-      if (e.deltaY) {
-        const direction = -1 * (e.deltaY / Math.abs(e.deltaY)); // either 1 or -1
-
-        let usedAxes;
-        if (e.ctrlKey && e.shiftKey)
-          usedAxes = ['z'];
-        else if (e.ctrlKey)
-          usedAxes = ['y'];
-        else if (e.shiftKey)
-          usedAxes = ['x'];
-        else
-          usedAxes = ['x', 'y', 'z'];
-
-        const axesAmounts = {};
-        for (const axis of usedAxes) {
-          axesAmounts[axis] = direction * that.mod.scale;
-        }
-
-        await that.animateStates(45, { scale: axesAmounts }, undefined, undefined, true);
-      }
-    }
-
-    function mouseMove(e) {
-      if (clickedBtn == 0) {
-        //LMB, translation
-        const distance = {
-          x: clickState.tran.x + (e.screenX - clickState.x) * that.mod.tran,
-          y: clickState.tran.y - (e.screenY - clickState.y) * that.mod.tran
-        };
-        if (distance.x || distance.y) {
-          that.assignNewStateAndDraw({
-            tran: distance
-          });
-        }
-      } else if (clickedBtn == 2) {
-        //RMB, rotation
-        //x and y are swapped because of the OpenGL 3D coordinate system axes
-        const distance = {
-          x: clickState.rot.x + (e.screenY - clickState.y) * that.mod.rot,
-          y: clickState.rot.y + (e.screenX - clickState.x) * that.mod.rot
-        };
-        if (distance.x || distance.y) {
-          that.assignNewStateAndDraw({
-            rot: distance
-          });
-        }
-      }
-    }
-
-    function mouseDown(e) {
-      if (e.button == 1) e.preventDefault();
-      clickedBtn = e.button;
-      clickState.x = e.screenX;
-      clickState.y = e.screenY;
-      clickState.tran = Object.assign({}, that.state.tran);
-      clickState.rot = Object.assign({}, that.state.rot);
-      window.addEventListener('pointermove', mouseMove);
-    }
-    function removeMouseMove() {
-      window.removeEventListener('pointermove', mouseMove);
+      window.addEventListener('gamepadconnected', this.gamepadConnected.bind(this));
+      window.addEventListener('gamepaddisconnected', this.gamepadDisconnected.bind(this));
     }
   }
 
-  // ---- Statics ----
-  // Mostly taken from https://easings.net/
-  Controls3D.Easing = {
-    LINEAR: x => x,
+  // ---- GamepadEvent functions ----
+  gamepadConnected(e) {
+    this._gamepads[e.gamepad.index] = e.gamepad;
+    this._hasGamepad = true;
+    this.gamepadLoop();
+  }
+  gamepadDisconnected(e) {
+    delete this._gamepads[e.gamepad.index];
+    if (Object.keys(this._gamepads).length == 0) {
+      this._hasGamepad = false;
+    }
+  }
 
-    EASE_IN_SINE: x => 1 - Math.cos((x * Math.PI) / 2),
-    EASE_IN_QUAD: x => x * x,
-    EASE_IN_CUBIC: x => x * x * x,
+  gamepadLoop() {
+    for (const gamepadIndex in this._gamepads) {
+      const gamepad = this._gamepads[gamepadIndex];
 
-    EASE_OUT_SINE: x => Math.sin((x * Math.PI) / 2),
-    EASE_OUT_QUAD: x => 1 - (1 - x) * (1 - x),
-    EASE_OUT_CUBIC: x => 1 - Math.pow(1 - x, 3),
+      // Scale, LB/RB
+      if (gamepad.buttons[4].pressed || gamepad.buttons[5].pressed) {
+        const direction = gamepad.buttons[5].value || -gamepad.buttons[4].value;
+        if (direction) {
+          this.animateStates(35, {
+            scale: {
+              x: direction * this.gamepadMod.scale,
+              y: direction * this.gamepadMod.scale,
+              z: direction * this.gamepadMod.scale
+            }
+          }, undefined, undefined, true);
+        }
+      }
 
-    EASE_IN_OUT_SINE: x => -(Math.cos(Math.PI * x) - 1) / 2,
-    EASE_IN_OUT_QUAD: x => x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2,
-    EASE_IN_OUT_CUBIC: x => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
+      // Translate, left joystick
+      this.handleJoystick([gamepad.axes[0], -gamepad.axes[1]], gamepad.buttons[10], 'tran');
 
-    EASE: x => {
-      return x < 0.2059
-        ? (5.2 * Math.pow(x, 1.8))
-        : (1 - 1.3 * Math.pow(1 - x, 2.7));
-    },
-  };
+      // Rotate, right joystick
+      this.handleJoystick([gamepad.axes[3], gamepad.axes[2]], gamepad.buttons[11], 'rot');
+    }
 
-  // ---- Prototype functions ----
-  Controls3D.prototype.animateStatesSteps = function(duration, animationSteps, ...args) {
+    if (this._hasGamepad) {
+      requestAnimationFrame(this.gamepadLoop);
+    }
+  }
+
+  handleJoystick(axes, resetButton, action) {
+    let hasNewState = false;
+    const newState = {};
+    newState[action] = {};
+
+    if (axes[0] > this.joystickThreshold || axes[0] < -this.joystickThreshold) {
+      hasNewState = true;
+      newState[action].x = (axes[0] * this.gamepadMod[action]) + this.state[action].x;
+    }
+    if (axes[1] > this.joystickThreshold || axes[1] < -this.joystickThreshold) {
+      hasNewState = true;
+      newState[action].y = (axes[1] * this.gamepadMod[action]) + this.state[action].y;
+    }
+
+    if (hasNewState) {
+      this.assignNewStateAndDraw(newState);
+    }
+
+    // Reset distance when right joystick is pressed
+    if (resetButton.pressed) {
+      newState[action].x = 0;
+      newState[action].y = 0;
+      this.assignNewStateAndDraw(newState);
+    }
+  }
+
+  // ---- MouseEvent functions ----
+  async wheel(e) {
+    if (e.ctrlKey) e.preventDefault();
+    if (e.deltaY) {
+      const direction = -1 * (e.deltaY / Math.abs(e.deltaY)); // either 1 or -1
+
+      let usedAxes;
+      if (e.ctrlKey && e.shiftKey)
+        usedAxes = ['z'];
+      else if (e.ctrlKey)
+        usedAxes = ['y'];
+      else if (e.shiftKey)
+        usedAxes = ['x'];
+      else
+        usedAxes = ['x', 'y', 'z'];
+
+      const axesAmounts = {};
+      for (const axis of usedAxes) {
+        axesAmounts[axis] = direction * this.mod.scale;
+      }
+
+      await this.animateStates(45, { scale: axesAmounts }, undefined, undefined, true);
+    }
+  }
+
+  mouseMove(e) {
+    if (this._clickedBtn == 0) {
+      //LMB, translation
+      const distance = {
+        x: this._clickState.tran.x + (e.screenX - this._clickState.x) * this.mod.tran,
+        y: this._clickState.tran.y - (e.screenY - this._clickState.y) * this.mod.tran
+      };
+      if (distance.x || distance.y) {
+        this.assignNewStateAndDraw({
+          tran: distance
+        });
+      }
+    } else if (this._clickedBtn == 2) {
+      //RMB, rotation
+      //x and y are swapped because of the OpenGL 3D coordinate system axes
+      const distance = {
+        x: this._clickState.rot.x + (e.screenY - this._clickState.y) * this.mod.rot,
+        y: this._clickState.rot.y + (e.screenX - this._clickState.x) * this.mod.rot
+      };
+      if (distance.x || distance.y) {
+        this.assignNewStateAndDraw({
+          rot: distance
+        });
+      }
+    }
+  }
+
+  mouseDown(e) {
+    if (e.button == 1) e.preventDefault();
+    this._clickedBtn = e.button;
+    this._clickState.x = e.screenX;
+    this._clickState.y = e.screenY;
+    this._clickState.tran = Object.assign({}, this.state.tran);
+    this._clickState.rot = Object.assign({}, this.state.rot);
+    window.addEventListener('pointermove', this.mouseMove);
+  }
+  removeMouseMove() {
+    window.removeEventListener('pointermove', this.mouseMove);
+  }
+
+  // ---- Animation ----
+  animateStatesSteps(duration, animationSteps, ...args) {
     animationSteps = Object.entries(animationSteps)
       .map(val => {
         val[0] = (Number(val[0]) / 100) * duration;
@@ -248,10 +231,11 @@ var Controls3D = (function() {
     });
   };
 
-  Controls3D.prototype.animateStates = function(
+  animateStates(
     duration, statesAmounts, drawCallback = this.drawFunction, easingFn = Controls3D.Easing.LINEAR, allowStacking
   ) {
     const that = this;
+
     return new Promise(resolve => {
       const usedStateNames = Object.keys(statesAmounts);
       let currentAnimationID = Infinity;
@@ -316,16 +300,38 @@ var Controls3D = (function() {
     }
   }
 
-  Controls3D.prototype.assignNewStateAndDraw = function(newState) {
+  assignNewStateAndDraw(newState) {
     this.assignNewState(newState);
     this.drawFunction();
   }
 
-  Controls3D.prototype.assignNewState = function(newState) {
+  assignNewState(newState) {
     for (const action in newState) {
       Object.assign(this.state[action], newState[action]);
     }
   }
 
-  return Controls3D;
-})();
+  // ---- Statics ----
+  // Mostly taken from https://easings.net/
+  static Easing = {
+    LINEAR: x => x,
+
+    EASE_IN_SINE: x => 1 - Math.cos((x * Math.PI) / 2),
+    EASE_IN_QUAD: x => x * x,
+    EASE_IN_CUBIC: x => x * x * x,
+
+    EASE_OUT_SINE: x => Math.sin((x * Math.PI) / 2),
+    EASE_OUT_QUAD: x => 1 - (1 - x) * (1 - x),
+    EASE_OUT_CUBIC: x => 1 - Math.pow(1 - x, 3),
+
+    EASE_IN_OUT_SINE: x => -(Math.cos(Math.PI * x) - 1) / 2,
+    EASE_IN_OUT_QUAD: x => x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2,
+    EASE_IN_OUT_CUBIC: x => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
+
+    EASE: x => {
+      return x < 0.2059
+        ? (5.2 * Math.pow(x, 1.8))
+        : (1 - 1.3 * Math.pow(1 - x, 2.7));
+    },
+  };
+}
